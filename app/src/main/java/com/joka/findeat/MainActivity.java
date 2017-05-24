01a544fd603ca25b4277c352e11cdf96c2524944
Johnny Xue <johnnyxue1998@gmail.com>
2017-05-24 15:37:22 -0600

    Added API key

--- app/src/main/java/com/joka/findeat/MainActivity.java
+++ app/src/main/java/com/joka/findeat/MainActivity.java
@@ -1,13 +1,40 @@
 package com.joka.findeat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.places.Places;
+
+public class MainActivity extends FragmentActivity
+        implements OnConnectionFailedListener {
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
     }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e("Error", "Connection Failed");
+        // TODO: Please implement GoogleApiClient.OnConnectionFailedListener to
+        // handle connection failures.
+
+    }
+
+
+
 }
